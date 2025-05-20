a167a18b17d7697d5ef89fe7472c3a158c7b7beb
rohitahemed <rohitzone884@gmail.com>
2025-05-20 23:32:57 +0600

    first commit

---
+++ option.js
@@ -0,0 +1,20 @@
+class product {
+    constructor(name,price,brand,malik){
+        this.name = name;
+        this.price = price;
+        this.brandNew = brand;
+        this.malik = malik; 
+
+    }
+ 
+}
+
+
+const iphone = new product('ipnone','23000','iphone', 'Rohit Ahemed');
+console.log(iphone);
+
+const xiomi = new product('redmi','18000','xiomi', 'Saimon');
+console.log(xiomi);
+
+const result = new product ('samsung','15000','sam','Habib');
+console.log(result);
